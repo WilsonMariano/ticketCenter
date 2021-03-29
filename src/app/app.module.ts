2db9269eeef746a3e4644953fc56a8025d7533dc
Mariano Wilson <mariano.wilson@bp-4.com>
2021-03-28 23:45:53 -0300

    First commit - Estructura del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
+import { NavbarModule } from './main/components/navbar/navbar.module';
+import { PagesModule } from './main/pages/pages.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PagesModule,
+    AppRoutingModule,
+    NavbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
