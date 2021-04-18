da1536136edb3e8a65287d3efbddab6bd79f0207
Mariano Wilson <mariano.wilson@bp-4.com>
2021-04-17 23:22:38 -0300

    Traigo las peliculas de firebase y las muestro en el home

--- src/app/main/components/movies-grid/movies-grid.component.ts
+++ src/app/main/components/movies-grid/movies-grid.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MoviesService } from './../../services/movies.service';
+import { Movie } from './../../classes/movie.class';
 
 @Component({
   selector: 'app-movies-grid',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesGridComponent implements OnInit {
 
-  constructor() { }
+  public movies: Movie[];
 
-  ngOnInit(): void {
+  constructor(private moviesService: MoviesService) { }
+
+  ngOnInit() {
+    this.getMovies();
+  }
+
+  private async getMovies() {
+    this.moviesService.getAll().subscribe(
+      res => this.movies = res);
   }
 
 }
--- src/app/main/pages/home/home.component.ts
+++ src/app/main/pages/home/home.component.ts
@@ -1,19 +1,12 @@
-import { MoviesService } from './../../services/movies.service';
-import { Component, OnInit } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent  {
 
-  constructor(private moviesService: MoviesService) { }
-
-  ngOnInit(): void {
-    const movies = this.moviesService.getAll().snapshotChanges().subscribe(data => console.log)
-
-  }
+  constructor() { }
 
 }
