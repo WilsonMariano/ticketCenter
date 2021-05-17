8b47b85f50ddb539585812cbae4c8573dab45ab1
Pablo Valenzuela <pablo86v@gmail.com>
2021-05-16 21:07:41 -0300

    Seleccion de funciones en progreso

--- src/app/main/pages/movie-detail/movie-detail.component.ts
+++ src/app/main/pages/movie-detail/movie-detail.component.ts
@@ -3,6 +3,7 @@ import { MoviesService } from './../../services/movies.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { FxGlobalsService } from '../../services/fx-globals.service';
 
 @Component({
   selector: 'app-movie-detail',
@@ -12,16 +13,24 @@ import { NgxSpinnerService } from 'ngx-spinner';
 export class MovieDetailComponent implements OnInit {
 
   public movie: Movie;
+  public selectedDate: String;
+  public initialDay: String;
+  public nextMovieShowsDays: String[] = [];
+  public movieShows: String[] = [];
+  private nextDaysQuantity: Number = 5;
 
   constructor(
     private route: ActivatedRoute,
     private moviesService: MoviesService,
-    private spinner: NgxSpinnerService) { }
+    private spinner: NgxSpinnerService,
+    private gFx: FxGlobalsService) { }
 
   ngOnInit(): void {
     const idMovie = parseInt(this.route.snapshot.paramMap.get('id'));
     console.log(idMovie);
     this.getMovie(idMovie);
+    this.setNextMovieShowsDays();
+    this.selectedDate = this.gFx.getDateDDMM(new Date());
   }
 
   private getMovie(idMovie: number): void {
@@ -30,11 +39,68 @@ export class MovieDetailComponent implements OnInit {
       res => {
         if(res.length === 1) {
           this.movie = res[0];
-          console.log(this.movie);
+          this.setMovieShows(res[0]);
         } 
         setTimeout(() => this.spinner.hide(), 1000);
       }
-    )
+    );
+  }
+
+  private setNextMovieShowsDays(){
+    const today = new Date();
+    const tomorrow = new Date();
+
+    for(let i=0;i < this.nextDaysQuantity; i++){
+      tomorrow.setDate(today.getDate() + i);
+      this.nextMovieShowsDays.push(this.gFx.getDayName(tomorrow) + " " + this.gFx.getDateDDMM(tomorrow));
+    }
+  }
+
+  // todo: La funcion debe poder prodesar por dia de la semana. Modificar los JSON
+  private setMovieShows(movie: Movie){
+    this.movie.movieShow.sort(function(a,b){
+      return getSortableDate(a.time).getTime() - getSortableDate(b.time).getTime();
+    });
+    this.movie.movieShow.forEach(ms => {
+      if(!this.hasBeenShowed(ms.time)){
+        this.movieShows.push(ms.time + " hs : " + ms.type);
+      }
+    });  
+  }
+
+  /**
+   * Indica si la función ya fue emitida, comparando con la hora actual
+   * @param movieShowTime 
+   * @returns Boolean
+   */
+  private hasBeenShowed(movieShowTime: string) : Boolean{
+    let d = new Date();
+    return (getSortableDate(movieShowTime).getTime() - d.getTime()) < 0;
+  }
+
+  private updateMovieShows(event: any){
+    let myDate = event.target.value;
+    this.selectedDate = myDate.substring(myDate.length -5) ;
+
+
+
   }
 
 }
+
+//****************FUNCIONES FUERA DE LA CLASE USADAS PARA CALLBACK   *************/
+
+/**
+ * A partir de un horario genera un objeto Date que pueda ser utilizado para ordenar un array.
+ * @param time en formato "hh:mm"
+ * @returns Objeto del tipo Date
+ */
+function getSortableDate(time: string) : Date{
+  let d = new Date();
+  d.setHours(parseInt(time.substring(0,2)));
+  d.setMinutes(parseInt(time.substring(3,6)));
+  return d;
+}
+
+ 
+
--- src/app/main/services/fx-globals.service.ts
+++ src/app/main/services/fx-globals.service.ts
@@ -27,4 +27,26 @@ export class FxGlobalsService {
   public hideSpinner(): void {
     setTimeout(() => this.spinnerService.hide(), 100);
   }
+
+  /**
+   * Devuelve el nombre del día , por ejemplo : "Lunes"
+   * @param d 
+   * @param inCapital Opcional, indica si la primer letra será devuelta en mayúscula. Default: true
+   * @returns String
+   */
+  public getDayName(d: Date, inCapital = true): String{
+    let dayName = d.toLocaleDateString("es-Es", { weekday: 'long' })
+    return inCapital ? this.firstToCapital(dayName) : dayName;
+  }
+
+  public getDateDDMM(d : Date): String{
+     let day = d.getDate() < 10 ? "0" + d.getDate() : d.getDate();
+    let month = d.getMonth() < 10 ? "0" + d.getMonth() : d.getMonth();
+    return  day.toString() + "/" + month.toString(); 
+  }
+
+  public firstToCapital(w: String): String{
+    return w.charAt(0).toUpperCase() + w.slice(1);
+  }
+
 }
